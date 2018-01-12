2e03da3602a0bb8896e12e87d1e3da9426a8d716
martin885 <mat885@hotmail.com>
2018-01-12 16:12:22 -0300

    Update App.xaml.cs

--- ControlGastos/ControlGastos/App.xaml.cs
+++ ControlGastos/ControlGastos/App.xaml.cs
@@ -23,7 +23,7 @@ namespace ControlGastos
 
             MainPage =new NavigationPage( new MenuTabbed());
 
-            OneSignal.Current.StartInit("d18e950f-8242-437d-beb8-28fc657cf0a4").EndInit();
+            OneSignal.Current.StartInit("").EndInit();
 
         }
 
